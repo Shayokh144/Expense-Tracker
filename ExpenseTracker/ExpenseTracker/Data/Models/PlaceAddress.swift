9d4391a069db57ab30eb01adfa4122150d875afe
Shayokh144 <shayokhbd144@gmail.com>
2024-01-16 00:39:15 +0700

    Added expensed list from ui to firebase

---
+++ ExpenseTracker/ExpenseTracker/Data/Models/PlaceAddress.swift
@@ -0,0 +1,17 @@
+//
+//  PlaceAddress.swift
+//  ExpenseTracker
+//
+//  Created by Taher on 10/1/24.
+//
+
+import Foundation
+
+struct PlaceAddress: Hashable {
+
+    let name: String
+    let city: String
+    let country: String
+    let longitude: Double?
+    let latitude: Double?
+}
---
+++ ExpenseTracker/ExpenseTracker/Extension/View+Extension.swift
@@ -0,0 +1,27 @@
+//
+//  View+Extension.swift
+//  ExpenseTracker
+//
+//  Created by Taher on 12/1/24.
+//
+
+import Combine
+import SwiftUI
+
+extension View {
+
+    var keyboardPublisher: AnyPublisher<Bool, Never> {
+        Publishers
+            .Merge(
+                NotificationCenter
+                    .default
+                    .publisher(for: UIResponder.keyboardWillShowNotification)
+                    .map { _ in true },
+                NotificationCenter
+                    .default
+                    .publisher(for: UIResponder.keyboardWillHideNotification)
+                    .map { _ in false })
+            .debounce(for: .seconds(0.1), scheduler: RunLoop.main)
+            .eraseToAnyPublisher()
+    }
+}
